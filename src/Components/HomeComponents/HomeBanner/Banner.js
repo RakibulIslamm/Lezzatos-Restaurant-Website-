f9b0f357ff403edc9a0a0c13a1a63e422846532a
RakibulIslam <rakibulislamsofficial@gmail.com>
2022-07-17 14:02:18 +0600

    Home Banner sec added

--- src/Components/HomeComponents/HomeBanner/Banner.js
+++ src/Components/HomeComponents/HomeBanner/Banner.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import bannerBg from '../../../images/home-images/italian-food-background-with-ingredients-GE9A25J-BG-Section.jpg'
+import indianLunch from '../../../images/home-images/indian-lunch-dinner-CLDWFXZ.jpg'
+import italianFood from '../../../images/home-images/traditional-italian-cookies-with-coconut-GX2FEKM-2-902x1024.jpg'
+// import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner</h2>
+        <div className='min-h-screen w-full bg-slate-300 bg-cover bg-center py-28 flex items-center justify-center' style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, .2), rgba(0, 0, 0, .6), rgba(0, 0, 0, 0)),url(${bannerBg})`
+        }}>
+            <div className='flex justify-between items-center py-28 max-w-[1920px] mx-auto'>
+                <div className='px-[100px] flex items-center justify-between gap-20 text-white w-full'>
+                    <div className='w-10/12'>
+                        <h3 className='font-dancing-script text-[36px]'>Welcome to Lezzatos</h3>
+                        <h1 className='text-[50px] leading-tight font-semibold py-3'>The Authentic<br />Restaurant & Cafe</h1>
+                        <p className='text-lg font-light inline-block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus doloribus mollitia unde. Obcaecati tempore in at molestias voluptas dicta laboriosam!</p>
+                        <button className='border-2 border-[#D7B686] px-8 py-2 mt-5 text-xl hover:bg-[#d7b686] hover:text-gray-800 transition-all ease-linear'>View More</button>
+                    </div>
+                    <div className='relative w-6/12'>
+                        <div className=' ml-auto block relative w-[336px] h-[396px] bg-white box-border' style={{
+                            clipPath: 'polygon(25% 0%, 100% 0, 100% 50%, 100% 100%, 0 100%, 0 25%)'
+                        }}>
+                            <img className='absolute top-[3px] left-[3px] w-[330px] h-[390px] object-cover' style={{
+                                clipPath: 'polygon(25% 0%, 100% 0, 100% 50%, 100% 100%, 0 100%, 0 25%)'
+                            }} src={indianLunch} alt="" />
+                        </div>
+
+
+                        <div className='absolute -bottom-28 right-48 inline-block'>
+                            <div className='inline-block relative w-[206px] h-[256px] bg-white box-border' style={{
+                                clipPath: 'polygon(0 1%, 80% 1%, 100% 20%, 100% 100%, 0 100%, 0 30%)'
+                            }}>
+                                <img className='absolute top-[3px] left-[3px] w-[200px] h-[250px] object-cover' style={{
+                                    clipPath: 'polygon(0 1%, 80% 1%, 100% 20%, 100% 100%, 0 100%, 0 30%)'
+                                }} src={italianFood} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Banner;
+
+// before:absolute before:w-0 before:h-0 before:border-t-[70px] before:border-t-white before:border-r-[70px] before:border-r-transparent
